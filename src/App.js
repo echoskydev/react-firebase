65c0e20dcc31ad604cbd64b8887d7efca67fbcd7
Echosky <echosky.dev@gmail.com>
2020-07-11 21:18:46 +0700

    create

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import firestore from "./database/firebase";
 
 function App() {
+
+  const [userName, setUserName] = useState("");
+  const [passWord, setPassWord] = useState("");
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+
+  const addUserHandler = (obj) => {
+    const ref = firestore.collection("users");
+    ref
+      .add(obj)
+      .then(() => {
+        console.log("add successfully.");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const obj = {
+      userName: userName,
+      passWord: passWord,
+      name: name,
+      age: age,
+    };
+
+    setUserName("");
+    setPassWord("");
+    setName("");
+    setAge("");
+
+    addUserHandler(obj);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ width: "80%", marginLeft: 20 }}>
+        <form onSubmit={submitHandler}>
+          <div>
+            <label>Username</label>
+            <input
+              type="text"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            ></input>
+          </div>
+          <div>
+            <label>Password</label>
+            <input
+              type="text"
+              value={passWord}
+              onChange={(e) => setPassWord(e.target.value)}
+            ></input>
+          </div>
+          <div>
+            <label>Name</label>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></input>
+          </div>
+          <div>
+            <label>Age</label>
+            <input
+              type="text"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+            ></input>
+          </div>
+          <button type="submit"> Add</button>
+        </form>
+      </div>
     </div>
   );
 }
