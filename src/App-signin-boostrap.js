d67d6e5b4e59a3d0b66b12d25acadf0be743743f
Echosky <echosky.dev@gmail.com>
2020-07-20 22:30:45 +0700

    login, register by email password

--- src/App-signin-boostrap.js
+++ src/App-signin-boostrap.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import LoginForm from "./component/LoginForm";
-
+import Header from "./component/Header";
 
 function App() {
   return (
     <div className="container">
-      <LoginForm></LoginForm>
+      <Header />
     </div>
   );
 }
---
+++ src/component/LoginForm.js
@@ -0,0 +1,248 @@
+import React, { useState, useEffect, useRef } from "react";
+import { auth, firesore } from "../database/firebase";
+
+export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [registerMode, setRegisterMode] = useState(false);
+  const [user, setUser] = useState(null);
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const userRef = useRef(firesore.collection("users")).current;
+  useEffect(() => {
+    const authUnsubscribe = auth.onAuthStateChanged((firebaseUser) => {
+      setLoading(true);
+      if (!!firebaseUser) {
+        userRef.doc(firebaseUser.uid).onSnapshot((doc) => {
+          if (doc.data()) {
+            const userData = {
+              uid: doc.data().uid,
+              displayName: doc.data().displayName,
+              photoURL: doc.data().photoURL,
+              email: doc.data().email,
+              created: doc.data().created,
+              role: doc.data().role,
+            };
+            setUser(userData);
+            setLoading(false);
+          }
+          clearForm();
+        });
+      } else {
+        setUser(null);
+      }
+    });
+    return () => {
+      authUnsubscribe();
+    };
+  }, [userRef]);
+
+  const onEmailLogin = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((result) => {
+        setLoading(false);
+      })
+      .catch((err) => {
+        setMessage(err.code);
+        setLoading(false);
+      });
+  };
+
+  const onEmailSignUp = (e) => {
+    e.preventDefault();
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(async (result) => {
+        setMessage("ลงทะเบียนเรียบร้อย");
+        if (!!result) {
+          const userRef = firesore.collection("users").doc(result.user.uid);
+          const doc = await userRef.get();
+          if (!doc.data()) {
+            await userRef.set({
+              uid: result.user.uid,
+              displayName: result.user.email.substring(
+                0,
+                email.lastIndexOf("@")
+              ),
+              photoURL: result.user.photoURL
+                ? result.user.photoURL
+                : require("../logo.svg"),
+              email: result.user.email,
+              created: new Date().valueOf(),
+              role: "user",
+            });
+          }
+        }
+        clearForm();
+      })
+      .catch((err) => {
+        setMessage(err.code);
+      });
+  };
+
+  const changeModeHandler = (e) => {
+    setRegisterMode(e.target.checked);
+    clearForm();
+  };
+
+  const clearForm = () => {
+    setEmail("");
+    setPassword("");
+    setMessage("");
+  };
+
+  const signOutHandler = () => {
+    auth
+      .signOut()
+      .then(() => {
+        console.log("Logout OK");
+      })
+      .catch((err) => {
+        console.log("Logout Not OK." + err);
+      });
+  };
+
+  if (!!user) {
+    return (
+      <div className="container-fluid">
+        {loading ? (
+          <div className="row">
+            <div className="col-sm-4 mx-auto d-flex justify-content-center">
+              <div className="spinner-grow text-danger m-1" role="status">
+                <span className="sr-only">Loading...</span>
+              </div>
+              <div className="spinner-grow text-warning m-1" role="status">
+                <span className="sr-only">Loading...</span>
+              </div>
+              <div className="spinner-grow text-info m-1" role="status">
+                <span className="sr-only">Loading...</span>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="row mt-1">
+            <div className="col-sm-10 mx-auto mt-3">
+              <div className="display-4 mt-3">สวัสดี {user.displayName}</div>
+              <img
+                src={user.photoURL}
+                alt="user"
+                width="30%"
+                className="rounded float-center mt-2"
+                style={{ maxHeight: "300" }}
+              />
+            </div>
+          </div>
+        )}
+        <div className="row mt-1">
+          <div className="col-sm-10 mx-auto mt-3 text-center">
+            <hr></hr>
+            <button className="btn btn-info" onClick={signOutHandler}>
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="container">
+      {message != "" ? (
+        <div className="row mt-1">
+          <div className="col-sm-6 mx-auto text-center">
+            <div className="alert alert-warning p-2">{message}</div>
+          </div>
+        </div>
+      ) : null}
+
+      <div className="row mt-1">
+        <div className="col-sm-6 mx-auto text-center">
+          <h3>{registerMode ? "Register" : "Login"}</h3>
+        </div>
+      </div>
+
+      <div className="row mt-1">
+        <form className="col-sm-6 mx-auto">
+          <div className="form-group">
+            <label HtmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              className="form-control"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              onFocus={() => setMessage("")}
+            />
+            <div className="invalid-feedback"></div>
+          </div>
+          <div className="form-group">
+            <label HtmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              className="form-control"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              onFocus={() => setMessage("")}
+            />
+            <div className="invalid-feedback"></div>
+          </div>
+
+          <div className="row card-footer flex-column-reverse flex-sm-row">
+            <div className="col-sm-6">
+              <button
+                className="btn btn-secondary mt-1"
+                style={{ width: "100%" }}
+              >
+                ยกเลิก
+              </button>
+            </div>
+
+            {!registerMode ? (
+              <div className="col-sm-6">
+                <button
+                  type="submit"
+                  className="btn btn-btn-primary mt-1"
+                  style={{ width: "100%" }}
+                  onClick={(e) => onEmailLogin(e)}
+                >
+                  Login
+                </button>
+              </div>
+            ) : (
+              <div className="col-sm-6">
+                <button
+                  type="submit"
+                  className="btn btn-btn-primary mt-1"
+                  style={{ width: "100%" }}
+                  onClick={(e) => onEmailSignUp(e)}
+                >
+                  Register
+                </button>
+              </div>
+            )}
+          </div>
+
+          <div className="form-check form-check-inline d-flex justify-content-center">
+            <div className="col-12 text-center">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                id="mode"
+                value={registerMode}
+                onChange={changeModeHandler}
+              ></input>
+              <label className="form-check-label" htmlFor="inlineCheckbox1">
+                  ยังไม่ได้ลงทะเบียน ต้องการลงทะเบียน
+              </label>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
