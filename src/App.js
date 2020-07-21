fb94faa5bdbd74f9453c3182e693aa282a62d238
Echosky <echosky.dev@gmail.com>
2020-07-21 14:08:38 +0700

    import header.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import LoginForm from "./component/LoginForm";
+import Header from "./component/Header";
 
 
 function App() {
   return (
     <div className="container">
+      <Header></Header>
       <LoginForm></LoginForm>
     </div>
   );
